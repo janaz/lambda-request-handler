55199e98a97fdc3345982689bffda8d33b03abe2
Tomasz Janowski <janaz9@gmail.com>
2019-06-24 11:51:26 +1000

    Remove unnecessary code

--- example/devServer.js
+++
@@ -1,2 +0,0 @@
-const app = require('./index');
-app.listen(3000, () => console.log('Listening on port 3000!'));
--- perf/handlerJNZ.js
+++
@@ -1,6 +0,0 @@
-const app = require('../example');
-const lambda = require('../dist/compile/lambda');
-
-const L = lambda(app);
-
-module.exports = L;
